a8416f94d601b7ecf5020ee7785eb6f8363f44c4
calex <christianrodriguezt@gmail.com>
2018-08-24 15:43:52 -0500

    Ecosistemas Quiz 1

--- app/src/main/java/com/edu/icesi/jess/MainActivity.java
+++ app/src/main/java/com/edu/icesi/jess/MainActivity.java
@@ -21,6 +21,7 @@ public Button btnMainToForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
